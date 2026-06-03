886ec8f68a6ce51651830a2dbc3c7b30efeba9eb
Daniel Machado <cdanielmachado@gmail.com>
2026-06-03 13:08:07 +0200

    update init files

---
+++ src/fluxbound/solver/__init__.py
@@ -0,0 +1,48 @@
+from ..core.model import Model
+from .solver import Solver
+
+available_solvers = dict()
+
+try:
+    from .scip_wrapper import SCIPSolver
+
+    available_solvers["scip"] = SCIPSolver
+except ImportError:
+    pass
+
+
+default_solver: str | None = None
+
+
+def get_default_solver() -> str:
+
+    global default_solver
+
+    if default_solver:
+        return default_solver
+
+    solver_order = ["gurobi", "cplex", "scip"]
+
+    for solver in solver_order:
+        if solver in list(available_solvers.keys()):
+            default_solver = solver
+            break
+
+    if not default_solver:
+        raise RuntimeError("No solver available.")
+
+    return default_solver
+
+
+def set_default_solver(solvername: str) -> None:
+
+    global default_solver
+
+    if solvername.lower() in list(available_solvers.keys()):
+        default_solver = solvername.lower()
+    else:
+        raise RuntimeError(f"Solver {solvername} not available.")
+
+
+def solver_instance(model: Model | None = None) -> Solver:
+    return available_solvers[get_default_solver()](model)
